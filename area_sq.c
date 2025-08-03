2c956fee69a4793f78894c10a7783e83b7a89fe1
Hari Sehgal <harisehgal34@gmail.com>
2025-08-03 22:24:41 +0530

    C Files

---
+++ area_sq.c
@@ -0,0 +1,10 @@
+// Area of square
+#include<stdio.h>
+
+int main(){
+    int side;
+    printf("Enter side of square");
+    scanf("%d",&side);
+    int area = side * side;
+    printf("Area of square %d",area);
+}
